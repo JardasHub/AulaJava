96306232fd1d43b3c94bfe43fff2c5063b144f02
JardasHub <jardel_c_oliveira@estudante.sesisenai.org.br>
2025-07-07 19:42:01 -0300

    Luís com acento

---
+++ src/Carro.java
@@ -0,0 +1,12 @@
+public class Carro {
+
+    long qtdRetrovisor = 0;
+    int qtdRodas = 0;
+    String cor = null;
+    double preco = 0.0;
+    boolean ligado = false;
+
+    public Carro(){
+        qtdRodas = 4;
+    }
+}
